1851fc66357b49b3f6f699942dad3b4765d611ae
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-02-07 20:29:21 -0600

    Fixed bug in config.py
    + MySQL port parameter must be an integer, so wrapped call of os.environ.get with cast to int

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv(dotenv_path)
 db_credentials = {
     'dev': {
         'host': os.environ.get('DB_HOST', 'localhost'),
-        'port': os.environ.get('DB_PORT', 3306),
+        'port': int(os.environ.get('DB_PORT', 3306)),
         'user': os.environ.get('DB_USERNAME'),
         'passwd': os.environ.get('DB_PASSWORD'),
         'db': os.environ.get('DB_DATABASE', 'fdm_test'),
