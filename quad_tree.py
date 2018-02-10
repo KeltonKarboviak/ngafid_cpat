4fdd271a69d16ab197c2ad95b2011d66a83f8d22
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-02-09 21:53:20 -0600

    In quad_tree.py, removed commented code & small refactor

--- quad_tree.py
+++ quad_tree.py
@@ -16,14 +16,11 @@ class QuadTree(object):
             'southwest': None,
             'southeast': None,
         }
-        # self._northwest, self._northeast, self._southwest, self._southeast = (None, None, None, None)
 
         self._airport = None
 
         lat_diff = fabs(lat_min - lat_max)
         lon_diff = fabs(lon_min - lon_max)
-
-        # print('lat_diff:', lat_diff, ' lon_diff:', lon_diff)
 
         if fabs(lat_min - lat_max) < self.MIN_GRID_SIZE and fabs(lon_min - lon_max) < self.MIN_GRID_SIZE:
             self._is_leaf = True
@@ -40,32 +37,23 @@ class QuadTree(object):
         return str(self)
 
     def get_lat_direction(self, lat):
-        if lat < self._lat_mid:
-            # South
-            return ('south', (self._lat_min, self._lat_mid))
-        else:
-            # North
-            return ('north', (self._lat_mid, self._lat_max))
+        return ('south', (self._lat_min, self._lat_mid))
+            if lat < self._lat_mid
+            else ('north', (self._lat_mid, self._lat_max))
 
     def get_lon_direction(self, lon):
-        if lon < self._lon_mid:
-            # West
-            return ('west', (self._lon_min, self._lon_mid))
-        else:
-            # East
-            return ('east', (self._lon_mid, self._lon_max))
+        return ('west', (self._lon_min, self._lon_mid))
+            if lon < self._lon_mid
+            else ('east', (self._lon_mid, self._lon_max))
 
     def insert(self, airport):
-        # print(self, end='\t')
-
         if self._is_leaf:
             self._airport = airport
         else:
-            lat_dir = self.get_lat_direction(airport.centerLatLon.lat)
-            lon_dir = self.get_lon_direction(airport.centerLatLon.lon)
+            lat_dir = self._get_lat_direction(airport.centerLatLon.lat)
+            lon_dir = self._get_lon_direction(airport.centerLatLon.lon)
 
             direction = lat_dir[0] + lon_dir[0]
-            # print('Going', direction, (lat_dir[1], lon_dir[1]))
 
             # Check if node hasn't been created already
             if not self._nodes[direction]:
@@ -73,39 +61,7 @@ class QuadTree(object):
 
             self._nodes[direction].insert(airport)
 
-            # if airport.centerLatLon.lat < self._lat_mid:
-            #     # South
-            #     direction += 'south'
-            #     if airport.centerLatLon.lon < self._lon_mid:
-            #         # West
-            #         direction += 'west'
-            #         print('southwest!')
-            #         self._southwest = QuadTree(self._lat_min, self._lat_mid, self._lon_min, self._lon_mid)
-            #         self._southwest.insert(airport)
-            #     else:
-            #         # East
-            #         direction += 'east'
-            #         print('southeast!')
-            #         self._southeast = QuadTree(self._lat_min, self._lat_mid, self._lon_mid, self._lon_max)
-            #         self._southeast.insert(airport)
-            # else:
-            #     # North
-            #     direction += 'north'
-            #     if airport.centerLatLon.lon < self._lon_mid:
-            #         # West
-            #         direction += 'west'
-            #         print('northwest!')
-            #         self._northwest = QuadTree(self._lat_mid, self._lat_max, self._lon_min, self._lon_max)
-            #         self._northwest.insert(airport)
-            #     else:
-            #         # East
-            #         direction += 'east'
-            #         print('northeast!')
-            #         self._northeast = QuadTree(self._lat_mid, self._lat_max, self._lon_mid, self._lon_max)
-            #         self._northeast.insert(airport)
-
     def get_nearest_airport(self, lat, lon):
-        # print(self, end='\t')
         if self._is_leaf:
             return self._airport
 
@@ -113,7 +69,6 @@ class QuadTree(object):
         lon_dir = self.get_lon_direction(lon)
 
         direction = lat_dir[0] + lon_dir[0]
-        # print('Going ', direction)
 
         try:
             airport = self._nodes[direction].get_nearest_airport(lat, lon)
