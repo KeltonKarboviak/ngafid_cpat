30fb7d7c554b121fd16752d85719031a315b5b16
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-02-11 20:56:06 -0600

    WIP, began refactoring main.py
    + Utilizing Pandas & NumPy for retrieving, manipulating, and storing data.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from __future__ import print_function
 import argparse
@@ -7,11 +7,14 @@ import logging
 import multiprocessing
 import MySQLdb as mysql
 import time
+import pandas as pd
+import numpy as np
 from config import db_credentials
 from Airport import Airport
 from FlightAnalysis import FlightAnalyzer
 from LatLon import LatLon
 from Runway import Runway
+from quad_tree import QuadTree
 
 
 """ LOGGING SETUP """
@@ -29,7 +32,7 @@ fetchFlightIDsSQL = "SELECT flight_id FROM flight_analyses WHERE approach_analys
 fetchAircraftTypeSQL = "SELECT aircraft_type FROM flight_id WHERE id = %s;"
 fetchFlightDataSQL = '''
     SELECT
-        time, msl_altitude, indicated_airspeed, vertical_airspeed, heading, latitude, longitude, pitch_attitude, eng_1_rpm
+        time, msl_altitude, derived_radio_altitude, indicated_airspeed, vertical_airspeed, heading, latitude, longitude, pitch_attitude, eng_1_rpm
     FROM
         main
     WHERE
@@ -38,117 +41,31 @@ fetchFlightDataSQL = '''
 '''
 
 """ GLOBAL VARIABLES """
-globalConn = None
-globalCursor = None
-globalFlightAnalyzer = None
+db = None
+cursor = None
+analyzer = None
 airports = {}
+quad_tree = None
 NUM_CPUS = multiprocessing.cpu_count()  # Set number of CPUs to use for multiprocessing
 
 
-class Consumer(multiprocessing.Process):
+def get_aircraft_type(flight_id: int) -> int:
+    cursor.execute(fetchAircraftTypeSQL, (flight_id,))
 
-    def __init__(self, task_queue, skipOutputToDB):
-        multiprocessing.Process.__init__(self)
-        self.task_queue = task_queue
-        self.conn = mysql.connect(**db_creds)
-        self.cursor = self.conn.cursor(mysql.cursors.DictCursor)
-        self.flightAnalyzer = FlightAnalyzer(self.conn, self.cursor, airports, skipOutput=skipOutputToDB)
-
-    def run(self):
-        while True:
-            next_task = self.task_queue.get()
-            if next_task is None:
-                print('Tasks Complete! Exiting ...')
-                self.task_queue.task_done()
-                break
-            answer = next_task(connection=self.conn, analyzer=self.flightAnalyzer)
-            self.task_queue.task_done()
+    return cursor.fetchone()['aircraft_type']
 
 
-class Task(object):
-
-    def __init__(self, flightID):
-        self.flightID = flightID
-
-    def __call__(self, connection=None, analyzer=None):
-        logging.info("Now Analyzing Flight ID [%s]", self.flightID)
-
-        cursor = connection.cursor(mysql.cursors.DictCursor)
-
-        try:
-            cursor.execute(fetchAircraftTypeSQL, (self.flightID,))
-            aircraftType = cursor.fetchone()['aircraft_type']
-
-            # Get the flight's data
-            cursor.execute(fetchFlightDataSQL, (self.flightID,))
-            rows = cursor.fetchall()
-
-            flightData = []
-            for row in rows:
-                # Before checking if flight data is valid, filter out data rows
-                # that contain NULL values
-                if None not in list(row.values()):
-                    row['LatLon'] = LatLon(row['latitude'], row['longitude'])
-                    flightData.append(row)
-
-            approaches = analyzer.analyze(
-                self.flightID,
-                aircraftType,
-                flightData,
-                skipAnalysis=False  # not isFlightDataValid(flightData[:10])
-            )
-
-            logging.info("Processing Complete Flight ID [%s]", self.flightID)
-        except mysql.Error as e:
-            logging.exception("MySQL Error [%d]: %s", e.args[0], e.args[1])
-            logging.exception("Last Executed Query: %s", cursor._last_executed)
-
-        return -1
+vector_create_latlons = np.vectorize(lambda lat, lon: LatLon(lat, lon))
 
 
-def main(flightIDs, runWithMultiProcess, skipOutputToDB):
-    '''
-    Main function gets a list of all the files contained within the passed in
-        folder name. Then scans through each file one-by-one in order to pass it
-        to the analyze data function to find approaches and landings.
-    After the data is analyzed, it then calls makeGraph to create the graph image.
-    @author: Wyatt Hedrick, Kelton Karboviak
-    '''
-    # If there are no flight_ids passed as command-line args,
-    # fetch all flights that haven't been analyzed for approaches yet
-    # Otherwise the ids passed into argv will only be analyzed
-    if len(flightIDs) == 0:
-        globalCursor.execute(fetchFlightIDsSQL)
-        flights = globalCursor.fetchall()
-        flightIDs = [flight['flight_id'] for flight in flights]
-        # flightIDs = [392706, 393230, 382486, 387607, 393246, 388639, 382496, 393769, 387627, 389165, 395316, 383544, 389178, 387765, 383556, 393289, 382538, 394766, 387160, 388186, 388192, 390247, 386666, 387181, 395374, 394355, 381046, 392824, 394362, 394365, 387201, 392836, 384647, 392334, 393837, 385690, 384674, 394927, 388638, 392886, 392898, 386765, 389844, 389850, 382172, 382178, 384307, 394475, 386800, 383219, 394998, 392955, 388354, 383749, 384269, 384270, 382741, 381218, 383781, 385836, 389421, 383790, 381233, 385331, 392504, 384326, 395599, 393554, 393046, 392538, 387949, 394933, 381812, 394127, 389521, 388498, 394645, 384412, 390048, 389027, 384420, 381349, 383403, 386486, 393655, 384441, 384445, 390082, 384965, 384460, 382928, 395219, 395220, 390052, 384476, 385645, 397800, 397803, 390131, 385012]
+def get_flight_data(flight_id: int) -> pd.DataFrame:
+    df = pd.read_sql(fetchFlightDataSQL, con=db, params=[flight_id])
 
-    logging.info('Number of Flights to Analyze: %4d', len(flightIDs))
+    # Create LatLon objects by grabbing the lat,lon columns, transpose so
+    # lat,lon becomes the rows, then unpack the numpy values array
+    df.loc[:, 'LatLon'] = vector_create_latlons(*df[['latitude', 'longitude']].T.values)
 
-    loadAirportData()
-
-    tasks = multiprocessing.JoinableQueue()
-
-    # If running in parallel, create NUM_CPUS number of Consumers for
-    #   processing tasks.
-    # If running linearly, only create 1 Consumer for processing tasks.
-    num_consumers = NUM_CPUS if runWithMultiProcess else 1
-    consumers = []
-    for i in range(num_consumers):
-        c = Consumer(tasks, skipOutputToDB)
-        c.start()
-        consumers.append(c)
-
-    # Push all the flight IDs onto the tasks Queue for processing
-    for flightID in flightIDs:
-        tasks.put(Task(flightID))
-
-    # Push None's onto Queue to signal to Consumers to stop consuming
-    for i in range(num_consumers):
-        tasks.put(None)
-
-    # Cause main thread to wait for queue to be empty
-    tasks.join()
+    return df
 
 
 def loadAirportData():
@@ -173,11 +90,31 @@ def loadAirportData():
             airports[row[2]].addRunway(r)  # Add runway to corresponding airport
 
 
-def isFlightDataValid(data):
-    for value in data:
-        if value['latitude'] not in (0, None) or value['longitude'] not in (0, None):
-            return True
-    return False
+def main(flightIDs, runWithMultiProcess, skipOutputToDB):
+    # If there are no flight_ids passed as command-line args,
+    # fetch all flights that haven't been analyzed for approaches yet
+    # Otherwise the ids passed into argv will only be analyzed
+    if len(flightIDs) == 0:
+        globalCursor.execute(fetchFlightIDsSQL)
+        flights = globalCursor.fetchall()
+        flightIDs = [flight['flight_id'] for flight in flights]
+
+    logging.info('Number of Flights to Analyze: %4d', len(flightIDs))
+
+    with stopwatch('Loading airport data'):
+        loadAirportData()
+
+    with stopwatch('Loading Quad Tree'):
+        # Load Airports into a QuadTree
+        quad_tree = QuadTree()
+        for k, v in airports.items():
+            quad_tree.insert(v)
+
+    flight_id = flightIDs[0]
+
+    aircraft_type_id = get_aircraft_type(flight_id)
+
+    flight_data = get_flight_data(flight_id)
 
 
 @contextlib.contextmanager
@@ -188,25 +125,20 @@ def stopwatch(msg):
         yield
     finally:
         t1 = time.time()
-    logger.info("Total elapsed time for %s: %.3f seconds", msg, t1 - t0)
+    print("Total elapsed time for %s: %.3f seconds" % (msg, t1 - t0))
+    # logger.info("Total elapsed time for %s: %.3f seconds", msg, t1 - t0)
 
 
-if __name__ == "__main__":
-    # Parse command-line args
-    parser = argparse.ArgumentParser(description='Tool to detect approaches in flight data.')
-    parser.add_argument('flight_ids', metavar='flight_id', type=str, nargs='*', help='a flight_id to be analyzed')
-    parser.add_argument('-m', '--multi-process', action='store_true', help='run program with multiple processes')
-    parser.add_argument('--no-write', action='store_true', help='program will not write results to DB')
-    args = parser.parse_args()
-
+if __name__ == '__main__':
     try:
-        globalConn = mysql.connect(**db_creds)
-        globalCursor = globalConn.cursor(mysql.cursors.DictCursor)
+        db = mysql.connect(**db_creds)
+        cursor = db.cursor(mysql.cursors.DictCursor)
 
-        with stopwatch("Program Execution"):
-            main(args.flight_ids, args.multi_process, args.no_write)
+        main()
     except mysql.Error as e:
-        print("MySQL Error [%d]: %s\n" % (e.args[0], e.args[1]))
+        print(e)
     finally:
-        if globalConn is not None:
-            globalConn.close()
+        if cursor is not None:
+            cursor.close()
+        if db is not None:
+            db.close()
