438ad1e52a7c7c0fe4be00b3ea712d3c42c37f2c
Unknown <kelton.karboviak@gmail.com>
2018-02-13 20:56:24 -0600

    In runway.py, type annotations & renamed variables
    + Renamed some constructor variables to lower-case for consistency.

--- runway.py
+++ runway.py
@@ -5,19 +5,29 @@ from latlon import LatLon
 
 class Runway(object):
 
-    def __init__(self, airportCode, alt, runwayCode, magHdg, trueHdg, lat, lon):
-        self.airportCode = airportCode
+    def __init__(
+        self,
+        airport_code: str,
+        alt: float,
+        runway_code: str,
+        mag_hdg: float,
+        true_hdg: float,
+        lat: float,
+        lon: float
+    ):
+        self.airportCode = airport_code
         self.alt = alt
-        self.runwayCode = runwayCode
-        self.magHeading = float(magHdg)
-        self.trueHeading = float(trueHdg)
+        self.runwayCode = runway_code
+        self.magHeading = float(mag_hdg)
+        self.trueHeading = float(true_hdg)
         self.centerLatLon = LatLon(float(lat), float(lon))
 
-    def __str__(self):
+    def __str__(self) -> str:
         return (
             '<Runway(airport={self.airportCode}, runway={self.runwayCode}, '
             'center={self.centerLatLon}, altitude={self.alt}, '
-            'mag_hdg={self.magHeading}, true_hdg={self.trueHeading})>'.format(self=self)
+            'mag_hdg={self.magHeading}, true_hdg={self.trueHeading})>'.format(
+                self=self)
         )
 
     def __repr__(self):
